32cc46f98e59b83566521a74847583fd67e24b48
Alenriquez96 <alberto.enriquez96@gmail.com>
2022-03-14 10:21:05 +0100

    usuarios creados

---
+++ app.js
@@ -0,0 +1,21 @@
+let nombre = document.getElementById("name");
+let pass = document.getElementById("password")
+
+let validacion = [
+    {
+        "nombre": "Daniel",
+        "pass:": "123456"
+    },
+    { 
+        "nombre": "Alberto",
+        "pass:": "123456"
+    },
+    {   
+        "nombre": "Jose",
+        "pass:": "123456"
+    },
+    {
+        "nombre": "Gaizka",
+        "pass:": "123456"
+    }
+        ];
