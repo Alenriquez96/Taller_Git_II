68a35f916937a8b30bca48261a34537cde70a874
Gaizka79 <agaizka@hotmail.com>
2022-03-14 10:50:37 +0100

    cambios js

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ let pass = document.getElementById("password")
 let validacion = [
     {
         "nombre": "Daniel",
-        "pass:": "123456"
+        "pass:": "123456D"
     },
     { 
         "nombre": "Alberto",
-        "pass:": "123456"
+        "pass:": "123456A"
     },
     {   
         "nombre": "Jose",
-        "pass:": "123456"
+        "pass:": "123456J"
     },
     {
         "nombre": "Gaizka",
-        "pass:": "123456"
+        "pass:": "123456G"
     }
         ];
